bb35d88873376bf344cf325a24331acc129c914a
ganesh <ganeshgajula22@gmail.com>
2021-04-28 23:10:15 +0530

    feat: added global error handler

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ app.get("/", (req, res) => {
   res.send("Welcome to the world of backend with express!!");
 });
 
+/**
+ * Error Handler
+ * Note: Do not move.
+ */
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    success: false,
+    message: "error occurred, see the error message for more details",
+    errMessage: err.Message,
+  });
+});
+
 app.listen(process.env.PORT || PORT, () => {
   console.log(`server is running at port ${PORT}`);
 });
