249bc393519b2d941d988fa5e72cf050594a9431
ganesh <ganeshgajula22@gmail.com>
2021-04-29 20:52:24 +0530

    add: routes for id param

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { Product } = require("../models/product.model");
+const { extend } = require("lodash");
 
 router
   .route("/")
@@ -28,6 +29,77 @@ router
         success: false,
         message: "couldn't add products",
         errorMessage: error.Message,
+      });
+    }
+  });
+
+router.param("productId", async (req, res, next, id) => {
+  try {
+    const matchedProduct = await Product.findById(id);
+
+    if (!matchedProduct) {
+      res.status(400).json({
+        success: false,
+        message: "The product you are trying to access is not available.",
+      });
+    }
+
+    req.product = matchedProduct;
+
+    next();
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message:
+        "Error while retrieving the product, kindly check the error message for more details",
+      errorMessage: error.message,
+    });
+  }
+});
+
+router
+  .route("/:productId")
+  .get(async (req, res) => {
+    try {
+      const { product } = req;
+      product.__v = undefined;
+      res.json({ success: true, product });
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message:
+          "Looks like something broke, kindly look into error message for more details",
+        errorMessage: error.message,
+      });
+    }
+  })
+  .post(async (req, res) => {
+    try {
+      const productUpdates = req.body;
+      let { product } = req;
+      product = extend(product, productUpdates);
+      const updatedProduct = await product.save();
+      res.json({ success: true, updatedProduct });
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message:
+          "Looks like something broke, kindly look into error message for more details",
+        errorMessage: error.message,
+      });
+    }
+  })
+  .delete(async (req, res) => {
+    try {
+      let { product } = req;
+      await product.remove();
+      res.json({ success: true, product });
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message:
+          "Looks like something broke, kindly look into the error message for more details",
+        errorMessage: error.message,
       });
     }
   });
