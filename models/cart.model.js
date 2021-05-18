7e3d59682d69313abb23b01b34cd5321175f9718
ganesh <ganeshgajula22@gmail.com>
2021-05-18 17:41:28 +0530

    add: cart model

---
+++ models/cart.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const CartSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  products: [
+    {
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+      quantity: Number,
+    },
+  ],
+});
+
+const Cart = mongoose.model("cart", CartSchema);
+
+module.exports = { Cart };
