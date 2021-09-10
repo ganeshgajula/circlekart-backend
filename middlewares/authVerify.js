8a07dbb40478eb13550862ac5f98d6c1cc76716f
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-09-10 13:22:08 +0530

    refactor: middlewares

---
+++ middlewares/authVerify.js
@@ -0,0 +1,18 @@
+const authVerify = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  console.log({ token });
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    console.log({ decoded });
+    req.user = { userId: decoded.userId };
+    return next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized access, please add the correct token.",
+      errorMessage: error.message,
+    });
+  }
+};
+
+module.exports = { authVerify };
---
+++ middlewares/routeHandler.js
@@ -0,0 +1,7 @@
+const routeHandler = (req, res) => {
+  res
+    .status(404)
+    .json({ success: false, message: "route not found on server" });
+};
+
+module.exports = { routeHandler };
