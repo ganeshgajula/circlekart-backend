59e81eb9e4b3ffd5c655f474036ff8b333a07605
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-09-10 13:41:33 +0530

    chore: jwt import

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
 const products = require("./routes/products.router");
 const users = require("./routes/users.router");
 const carts = require("./routes/carts.router");
