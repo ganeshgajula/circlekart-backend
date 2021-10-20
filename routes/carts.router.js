cf3635f9bae92a38fe48bd04c638d26fc6641966
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-10-20 17:27:19 +0530

    feat: checkout route

--- routes/carts.router.js
+++ routes/carts.router.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { extend } = require("lodash");
 const { User } = require("../models/user.model");
 const { Cart } = require("../models/cart.model");
+const Razorpay = require("razorpay");
+const { nanoid } = require("nanoid");
 
 router.param("userId", async (req, res, next, id) => {
   try {
@@ -95,5 +97,35 @@ router
       });
     }
   });
+
+router.route("/checkout").post(async (req, res) => {
+  try {
+    const { orderInfo } = req.body;
+
+    const instance = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET,
+    });
+
+    const options = {
+      amount: orderInfo.totalAmount * 100,
+      currency: "INR",
+      receipt: nanoid(),
+      payment_capture: 1,
+    };
+
+    const order = await instance.orders.create(options);
+    console.log(order);
+
+    res.status(200).json({ success: true, order });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message:
+        "Couldn't complete transaction, kindly check the error message for more details",
+      errorMessage: error.message,
+    });
+  }
+});
 
 module.exports = router;
